7854a2528b0f8d2850d447be162d70c42d129ca0
小斯（xiaosi） <1531733886@qq.com>
2023-05-15 23:26:09 +0800

    feat: ✨ build

---
+++ docs/.vitepress/dist/assets/guide_function_deepEqual.md.a6e7253b.lean.js
@@ -0,0 +1,13 @@
+import { o as e, g as a, h as s } from './app.717403ec.js';
+const t = s('', 8),
+  l = [t],
+  r = JSON.parse(
+    '{"title":"深度匹配器","description":"","frontmatter":{"outline":"deep"},"headers":[{"level":2,"title":"deepEqual","slug":"deepequal","link":"#deepequal","children":[]}],"relativePath":"guide/function/deepEqual.md","lastUpdated":1682932221000}',
+  ),
+  o = { name: 'guide/function/deepEqual.md' },
+  i = Object.assign(o, {
+    setup(n) {
+      return (p, d) => (e(), a('div', null, l));
+    },
+  });
+export { r as __pageData, i as default };
